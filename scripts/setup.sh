945311c3f551386690ba0172ee1e748ae0db8319
env. <162432464+belowzeroiq@users.noreply.github.com>
2025-12-11 22:48:48 +0700

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 sudo apt update && sudo apt install -y \
-    bc bison build-essential ccache curl flex \
-    g++-multilib gcc-multilib git gnupg gperf \
-    lib32readline-dev lib32z1-dev \
-    liblz4-tool libncurses5-dev libsdl1.2-dev \
-    libssl-dev libxml2 libxml2-utils \
-    zstd pigz lzop \
-    rsync schedtool squashfs-tools zip \
-    android-tools-adb android-tools-fastboot \
-    python-is-python3 python3 python3-pip jq \
-    repo \
-    git-lfs \
-    android-sdk-platform-tools-common
+  bc bison build-essential ccache curl \
+  flex g++-multilib gcc-multilib git git-lfs \
+  gnupg gperf imagemagick lib32ncurses-dev lib32readline-dev \
+  lib32z1-dev liblz4-tool libncurses6 libncurses-dev \
+  libsdl1.2-dev libssl-dev libwxgtk3.2-dev libxml2 libxml2-utils \
+  lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev \
+  git repo wget jq \
+  python-is-python3 aria2
 
 sudo apt upgrade -y
+
+wget http://security.ubuntu.com/ubuntu/pool/universe/n/ncurses/libtinfo5_6.2-0ubuntu2_amd64.deb && sudo dpkg -i libtinfo5_6.2-0ubuntu2_amd64.deb && rm -rf libtinfo5_6.2-0ubuntu2_amd64.deb
+
+wget https://github.com/xordinaryme/builder/releases/download/v1/libncurses5_6.2-0ubuntu2_amd64.deb && sudo dpkg -i libncurses5_6.2-0ubuntu2_amd64.deb && rm -rf libncurses5_6.2-0ubuntu2_amd64.deb
 
 echo "Setting Up AOSP Build Environment"

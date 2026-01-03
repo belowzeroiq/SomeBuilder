4ae3e1b97eadeefd24fdb89d4744bb42d8c29c0d
env. <162432464+belowzeroiq@users.noreply.github.com>
2026-01-03 07:10:12 -0800

    Update device_requirements.sh

--- scripts/device_requirements.sh
+++ scripts/device_requirements.sh
@@ -2,13 +2,6 @@
 
 set_requirements() {
   clear
-  mkdir out
-  mkdir out/target
-  mkdir out/target/product
-  mkdir out/target/product/tapas
-  mkdir out/target/product/tapas/obj
-  mkdir out/target/product/tapas/obj/KERNEL_OBJ
-  mkdir out/target/product/tapas/obj/KERNEL_OBJ/usr
 }
 
 set_requirements
